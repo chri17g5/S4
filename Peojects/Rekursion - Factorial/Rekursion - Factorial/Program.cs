ca5f5e47a4a7185242b4d139c91a945035f6c6d5
Christian Weirum <Christian.Weirum@outlook.dk>
2021-11-15 11:50:00 +0100

    First day of S4.2!
    Created the project folder.
    Finished, first exersice.
    Started, second exersice

---
+++ Peojects/Rekursion - Factorial/Rekursion - Factorial/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Rekursion___Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                //  Get userinput
+                Console.WriteLine("<< Please enter a value of = n >>");
+                string userInput = Console.ReadLine();
+
+                //  Try catch manly so the program doesn't crash if it fails to parse the int
+                try
+                {
+                    int userNum = int.Parse(userInput);
+                    Console.WriteLine(Factorial(userNum));  //  Call Factorial function and write it!
+                }
+                catch
+                {
+                    //  Error message
+                    Console.WriteLine("<< ERROR: Input was not an interger >>");
+                }
+            }
+        }
+
+        static int Factorial(int number)
+        {
+            if (number == 1)    //  Ends the recursion if the number is one
+                return 1;
+
+            //  Returns the factorial number 
+            return number * Factorial(number - 1);  //  Too large numbers create a stack overflow error
+        }
+    }
+}
