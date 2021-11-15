dd95252bab585f449ae000df25462ec57eb40bb8
Christian Weirum <Christian.Weirum@outlook.dk>
2021-11-15 14:20:41 +0100

    Fibinacci finished

--- Peojects/Rekrusion - Fibonacci/Rekrusion - Fibonacci/Program.cs
+++ Peojects/Rekrusion - Fibonacci/Rekrusion - Fibonacci/Program.cs
@@ -6,7 +6,50 @@ namespace Rekrusion___Fibonacci
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                //  Get userinput
+                Console.WriteLine("<< Please enter a value of = n >>");
+                string userInput = Console.ReadLine();
+
+                //  Try catch mainly so the program doesn't crash, if it fails to parse the int
+                try
+                {
+                    int userNum = int.Parse(userInput);
+                    WriteFibonacci(userNum);  //  Call Factorial function and write it!
+                }
+                catch
+                {
+                    //  Error message
+                    Console.WriteLine("<< ERROR: Input was not an interger >>");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Prints out given Fibonacci que number.
+        /// </summary>
+        /// <param name="queNumber"></param>
+        /// <returns></returns>
+        static int Fibonacci(int queNumber)
+        {
+            if (queNumber <= 2)
+                return 1;
+            
+            return Fibonacci(queNumber - 1) + Fibonacci(queNumber - 2);
+        }
+
+        /// <summary>
+        /// Writes a Fibonacci value untill it hits "count" ammount
+        /// </summary>
+        /// <param name="count">Amount of times it has to count</param>
+        static void WriteFibonacci(int count)
+        {
+            if (count > 1)
+            {
+                WriteFibonacci(count - 1);
+            }
+            Console.WriteLine(Fibonacci(count));
         }
     }
 }
