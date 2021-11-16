ffced826df38f88f6492db46f39e5aec87fe921b
Christian Weirum <Christian.Weirum@outlook.dk>
2021-11-16 12:09:05 +0100

    Folder search program finished

---
+++ Projects/Relrusion - FolderSearch/Relrusion - FolderSearch/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+namespace Relrusion___FolderSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo directory = new DirectoryInfo(@"C:\Users\chri17g5\Pictures");
+            FileInfo[] files = directory.GetFiles("*");
+
+            WriteFiles(files.Length, files);
+        }
+
+        static void WriteFiles(int count, FileInfo[] file)
+        {
+            try
+            {
+                if (count >= 1)
+                    WriteFiles(count - 1, file);
+
+                Console.WriteLine(file[count].Name);
+            }
+            catch 
+            {
+                
+            }
+        }
+    }
+}
